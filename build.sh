c999202b670d0a590bb785663d4b382147a47296
Tomasz Żmuda <mundek@test.xabe>
2017-07-25 21:28:25 +0200

    Assembly version

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ dotnet test Xabe.FFMpeg.Test/
 if [[ -z "${TRAVIS_TAG}" ]]; then 
 	exit 0
 else
-	dotnet clean Xabe.FFMpeg -c Release
-	dotnet build Xabe.FFMPeg -c Release /property:Version=$TRAVIS_TAG
-	dotnet pack Xabe.FFMpeg --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG
-	dotnet nuget push Xabe.FFMpeg/nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
+	cd Xabe.FFMpeg
+	dotnet clean -c Release
+	dotnet build -c Release /property:Version=$TRAVIS_TAG
+	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG
+	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
