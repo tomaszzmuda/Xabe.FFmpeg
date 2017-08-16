9d6acaf50159862af9f47276b0b5a2a0890cf119
tzmuda <tomasz.a.zmuda@gmail.com>
2017-08-16 21:23:48 +0200

    Missing access modifier

--- Xabe.FFMpeg/FFBase.cs
+++ Xabe.FFMpeg/FFBase.cs
@@ -11,7 +11,7 @@ namespace Xabe.FFMpeg
     /// <summary>
     ///     Base FFMpeg class
     /// </summary>
-    abstract class FFBase: IDisposable
+    public abstract class FFBase: IDisposable
     {
         /// <summary>
         ///     Directory contains FFMpeg and FFProbe
