7a4bf0b7c03d6ebf41f11dcdad78b5f4c5a5cfa4
tzmuda <tomasz.a.zmuda@gmail.com>
2017-08-17 23:50:09 +0200

    Generate documentation

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ else
 	cd Xabe.FFMpeg
 	dotnet clean -c Release
 	dotnet build -c Release /property:Version=$TRAVIS_TAG
-	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG
+	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
