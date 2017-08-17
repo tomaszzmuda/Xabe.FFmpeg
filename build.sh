56c52fd61e0c2d720fd0cc09fdc6438a33fd6419
tzmuda <tomasz.a.zmuda@gmail.com>
2017-08-17 23:56:42 +0200

    Build fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [[ -z "${TRAVIS_TAG}" ]]; then
 else
 	cd Xabe.FFMpeg
 	dotnet clean -c Release
-	dotnet build -c Release /property:Version=$TRAVIS_TAG
+	dotnet build -c Release /property:Version=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
