a41dbf9957d6752821bbe404027eab94d30529a2
tzmuda <tomasz.a.zmuda@gmail.com>
2017-09-03 16:32:59 +0200

    Comment unused properties in ffprobe model

--- Xabe.FFMpeg/Model/Format.cs
+++ Xabe.FFMpeg/Model/Format.cs
@@ -20,24 +20,26 @@ namespace Xabe.FFMpeg.Model
         [UsedImplicitly]
         internal class Format
         {
-            public string filename { get; set; }
-            public int nbStreams { get; set; }
-            public int nbPrograms { get; set; }
-            public string formatName { get; set; }
-            public string formatLongName { get; set; }
-            public string startTime { get; set; }
-            public double duration { get; set; }
-            public string size { get; set; }
-            public double bitRate { get; set; }
-            public int probeScore { get; set; }
-            public Tags tags { get; set; }
+//            NOT USED NOW BUT THIS VALUES IS STILL RETURNED IN FFPROBE OUTPUT
+//            public string filename { get; set; }
+//            public int nbStreams { get; set; }
+//            public int nbPrograms { get; set; }
+//            public string formatName { get; set; }
+//            public string formatLongName { get; set; }
+//            public string startTime { get; set; }
+//            public string size { get; set; }
+//            public int probeScore { get; set; }
+//            public Tags tags { get; set; }
+            public double bitRate { get; [UsedImplicitly] set; }
+            public double duration { get; [UsedImplicitly] set; }
         }
 
-        [UsedImplicitly]
-        internal class Tags
-        {
-            public string encoder { get; set; }
-            public string creationTime { get; set; }
-        }
+//            NOT USED NOW BUT THIS VALUES IS STILL RETURNED IN FFPROBE OUTPUT
+//        [UsedImplicitly]
+//        internal class Tags
+//        {
+//            public string encoder { get; set; }
+//            public string creationTime { get; set; }
+//        }
     }
 }
