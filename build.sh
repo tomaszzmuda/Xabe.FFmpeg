8488c6038310e2b80a78405a940cec76dd20cc07
tzmuda <tomasz.a.zmuda@gmail.com>
2017-09-16 12:18:53 +0200

    Build test

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ fi
 dotnet build -c Release /property:Version=$version /p:GenerateDocumentationFile=true
 dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$version /p:GenerateDocumentationFile=true
 
-if [[ -z "${$NUGET_API}" ]]; then 
+if [[ -z "${NUGET_API}" ]]; then 
+	exit 0
+else
 	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
