3138e1fb103b05cd9552893ccf9cce2ca824a9bb
tzmuda <tomasz.a.zmuda@gmail.com>
2017-09-16 12:24:45 +0200

    Build test

--- build.sh
+++ build.sh
@@ -4,16 +4,10 @@ dotnet test Xabe.FFmpeg.Test/
 dotnet clean -c Release
 cd Xabe.FFmpeg
 if [[ -z "${TRAVIS_TAG}" ]]; then 
-	version=1.0.0
+	dotnet build /property:Version=1.0.0 /p:GenerateDocumentationFile=true
+	dotnet pack --no-build -o nuget /p:PackageVersion=$version /p:GenerateDocumentationFile=true
 else
-	version=$TRAVIS_TAG
-fi
-
-dotnet build -c Release /property:Version=$version /p:GenerateDocumentationFile=true
-dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$version /p:GenerateDocumentationFile=true
-
-if [[ -z "${NUGET_API}" ]]; then 
-	exit 0
-else
+	dotnet build -c Release /property:Version=$TRAVIS_TAG /p:GenerateDocumentationFile=true
+	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
 fi
