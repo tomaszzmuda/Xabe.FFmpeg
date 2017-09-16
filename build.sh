a659966c03f8c8ba16f399ddd43c26dd6ecd6782
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2017-09-16 12:59:01 +0200

    Change build (#9)
    * Build test
    
    * Delete singing
    
    * Build test
    
    * Build test
    
    * Build test
    
    * powershell build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 dotnet restore
 dotnet test Xabe.FFmpeg.Test/
+dotnet clean -c Release
+cd Xabe.FFmpeg
 if [[ -z "${TRAVIS_TAG}" ]]; then 
-	exit 0
+	dotnet build /property:Version=1.0.0 /p:GenerateDocumentationFile=true
+	dotnet pack --no-build -o nuget /p:PackageVersion=$version /p:GenerateDocumentationFile=true
 else
-	cd Xabe.FFmpeg
-	dotnet clean -c Release
 	dotnet build -c Release /property:Version=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet pack --no-build -c Release -o nuget /p:PackageVersion=$TRAVIS_TAG /p:GenerateDocumentationFile=true
 	dotnet nuget push nuget/*.nupkg -k $NUGET_API -s https://www.nuget.org/api/v2/package
