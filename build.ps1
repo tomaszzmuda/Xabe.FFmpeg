ae4b96fb8f7e94fbdec6ba8f3fd35b41c1c0dfee
tzmuda <tomasz.a.zmuda@gmail.com>
2017-09-28 14:38:58 +0200

    Update ikony

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 #!/bin/bash
 dotnet restore
 cd Xabe.FFmpeg
-dotnet build /property:Version=1.0.0 /p:GenerateDocumentationFile=true
-dotnet pack --no-build -o ../ /p:PackageVersion=$version /p:GenerateDocumentationFile=true
+dotnet build /p:GenerateDocumentationFile=true
+dotnet pack --no-build /p:GenerateDocumentationFile=true
