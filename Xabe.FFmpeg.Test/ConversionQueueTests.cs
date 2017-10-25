1b9b5d7bcba8439c745cfc916ff353f0de54e0e5
tzmuda <tomasz.a.zmuda@gmail.com>
2017-10-25 21:03:25 +0200

    Queue

---
+++ Xabe.FFmpeg.Test/ConversionQueueTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xabe.FFmpeg.Enums;
+using Xunit;
+
+namespace Xabe.FFmpeg.Test
+{
+    public class ConversionQueueTests
+
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task QueueTest(bool parallel)
+        {
+            var queue = new ConversionQueue(parallel);
+            var outputs = new List<string>();
+            var conversions = new List<IConversion>();
+
+            for(var i = 0; i < 2; i++)
+            {
+                string output = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + Extensions.Mp4);
+                outputs.Add(output);
+                IConversion conversion = ConversionHelper.ToMp4(Resources.MkvWithAudio, output, multithread:!parallel);
+                conversions.Add(conversion);
+                queue.Add(conversion);
+            }
+
+            await queue.Start();
+
+            foreach(string output in outputs)
+            {
+                var mediaInfo = new MediaInfo(output);
+                Assert.Equal(TimeSpan.FromSeconds(9), mediaInfo.Properties.Duration);
+                Assert.Equal("h264", mediaInfo.Properties.VideoFormat);
+                Assert.Equal("aac", mediaInfo.Properties.AudioFormat);
+            }
+        }
+    }
+}
---
+++ Xabe.FFmpeg/ConversionQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Xabe.FFmpeg
+{
+    /// <summary>
+    ///     Create queue for conversions
+    /// </summary>
+    public class ConversionQueue
+    {
+        private readonly bool _parallel;
+        private readonly ConcurrentQueue<IConversion> _queue = new ConcurrentQueue<IConversion>();
+        private readonly List<Task> _tasks = new List<Task>();
+
+        /// <inheritdoc />
+        public ConversionQueue(bool parallel = false)
+        {
+            _parallel = parallel;
+        }
+
+        private async Task Worker()
+        {
+            while(_queue.TryDequeue(out IConversion conversion))
+                await conversion.Start();
+        }
+
+        /// <summary>
+        ///     Start conversions
+        /// </summary>
+        public async Task Start()
+        {
+            if (!_parallel)
+                _tasks.Add(Worker());
+            else
+                for (var i = 0; i < Environment.ProcessorCount; i++)
+                    _tasks.Add(Worker());
+
+            await Task.WhenAll(_tasks);
+        }
+
+        /// <summary>
+        ///     Add conversion to queue
+        /// </summary>
+        /// <param name="conversion">Defined conversion</param>
+        public void Add(IConversion conversion)
+        {
+            _queue.Enqueue(conversion);
+        }
+    }
+}
