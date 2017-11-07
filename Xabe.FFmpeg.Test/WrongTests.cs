d61361682c7e3a7e9e1e34fa4ec0b6aad509e46b
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2017-11-07 19:58:00 +0100

    Add wrong unit test

---
+++ Xabe.FFmpeg.Test/WrongTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Xabe.FFmpeg.Test
+{
+    public class WrongTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(false);
+        }
+    }
+}
