0d27c04457ec510d0687117b755d64cf77200291
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2017-11-07 20:28:29 +0100

    Wrong unit test

--- Xabe.FFmpeg.Test/WrongTests.cs
+++ Xabe.FFmpeg.Test/WrongTests.cs
@@ -1,16 +1,13 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using Xunit;
-//
-//namespace Xabe.FFmpeg.Test
-//{
-//    public class WrongTests
-//    {
-//        [Fact]
-//        public void Test()
-//        {
-//            Assert.True(false);
-//        }
-//    }
-//}
+﻿using Xunit;
+
+namespace Xabe.FFmpeg.Test
+{
+    public class WrongTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(false);
+        }
+    }
+}
