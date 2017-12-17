10c1f891d162942e5cc449ce9b1e11954dae62b6
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2017-12-17 15:21:20 +0100

    Test fix

--- Xabe.FFMpeg/EnumExtensions.cs
+++ Xabe.FFMpeg/EnumExtensions.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 
 namespace Xabe.FFmpeg
 {
-    internal static class EnumExtensions
+    public static class EnumExtensions
     {
 
-        internal static string GetDescription(this Enum value)
+        public static string GetDescription(this Enum value)
         {
             FieldInfo fi = value.GetType()
                                 .GetField(value.ToString());
