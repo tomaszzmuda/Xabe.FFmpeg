92f60d4c8a726b428904aa73b11b39cc95973306
Łukasz Kischler <mrkisiel@live.com>
2018-03-23 21:08:38 +0100

    setting overwritte output parameter

--- Xabe.FFmpeg/Conversion/IConversion.cs
+++ Xabe.FFmpeg/Conversion/IConversion.cs
@@ -47,6 +47,13 @@ namespace Xabe.FFmpeg
         IConversion SetOutput(string outputPath);
 
         /// <summary>
+        ///     Set overwrite output file parameter
+        /// </summary>
+        /// <param name="overwrite">Should be output file overwritten or not</param>
+        /// <returns>>IConversion object</returns>
+        IConversion SetOverwriteOutput(bool overwrite);
+
+        /// <summary>
         ///     Fires when FFmpeg progress changes
         /// </summary>
         event ConversionProgressEventHandler OnProgress;
