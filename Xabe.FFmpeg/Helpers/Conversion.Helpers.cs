152cd0704aea824bd43dd071e4aefe6c947fbef6
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2018-07-17 20:46:47 +0200

    Convert with hardware

--- Xabe.FFmpeg/Helpers/Conversion.Helpers.cs
+++ Xabe.FFmpeg/Helpers/Conversion.Helpers.cs
@@ -380,18 +380,19 @@ namespace Xabe.FFmpeg
         }
 
         /// <summary>
-        ///     Convert one file to another with destination format using hardware acceleration (if possible). Using cuvid. Requires Windows or Linux and NVidia GPU.
+        ///     Convert one file to another with destination format using hardware acceleration (if possible). Using cuvid. Works only on Windows/Linux with NVidia GPU.
         /// </summary>
         /// <param name="inputFilePath">Path to file</param>
         /// <param name="outputFilePath">Path to file</param>
+        /// <param name="hardwareAccelerator">Hardware accelerator. List of all acceclerators avaiable for your system - "ffmpeg -hwaccels"</param>
         /// <param name="decoder">Codec using to decoding input video (e.g. h264_cuvid)</param>
         /// <param name="encoder">Codec using to encode output video (e.g. h264_nvenc)</param>
+        /// <param name="device">Number of device (0 = default video card) if more than one video card.</param>
         /// <returns>IConversion object</returns>
-        public static IConversion ConvertWithHardware(string inputFilePath, string outputFilePath, VideoCodec decoder, VideoCodec encoder)
+        public static IConversion ConvertWithHardware(string inputFilePath, string outputFilePath, HardwareAccelerator hardwareAccelerator, VideoCodec decoder, VideoCodec encoder, int device = 0)
         {
             var conversion = Convert(inputFilePath, outputFilePath);
-            conversion.UseHardwareAcceleration(HardwareAccelerator.cuvid, decoder, 0);
-            conversion.AddParameter($"-c:v {encoder?.ToString()} ");
+            conversion.UseHardwareAcceleration(hardwareAccelerator, decoder, encoder, device);
             return conversion;
         }
     }
