a32fdaa19e6eda87d7f1d327bc1446eeff55d231
Łukasz Kischler <mrkisiel@gmail.com>
2018-09-20 13:11:16 +0200

    Sending own arguments to ffprobe

--- Xabe.FFmpeg.Test/ProbeTests.cs
+++ Xabe.FFmpeg.Test/ProbeTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Xabe.FFmpeg.Exceptions;
 using Xunit;
 
 namespace Xabe.FFmpeg.Test
@@ -9,7 +8,7 @@ namespace Xabe.FFmpeg.Test
     public class ProbeTests
     {
         [Fact]
-        public async Task StartTest()
+        public async Task StartWithCsvResultTest()
         {
             string result = await Probe.New()
                  .Start($"-loglevel error -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time -of csv=print_section=0 {Resources.Mp4}");
@@ -18,6 +17,15 @@ namespace Xabe.FFmpeg.Test
                                .Where(x => !string.IsNullOrEmpty(x));
 
             Assert.Equal(3, values.Count());
+        }
+
+        [Fact]
+        public async Task StartWithStdOutputTest()
+        {
+            string result = await Probe.New()
+                                       .Start($"-loglevel error -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time {Resources.Mp4}");
+            
+            Assert.True(!string.IsNullOrEmpty(result));
         }
     }
 }
