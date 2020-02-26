eb6fcbc9c8f5571c3f3dde8fbb6f236295e75af8
Tomasz Żmuda <tomasz.a.zmuda@gmail.com>
2020-02-26 08:47:40 +0100

    Add aspect parameter to Cocatenate method

--- Xabe.FFmpeg/Helpers/Conversion.Helpers.cs
+++ Xabe.FFmpeg/Helpers/Conversion.Helpers.cs
@@ -348,13 +348,13 @@ namespace Xabe.FFmpeg
                 conversion.AddParameter(
                     $"[{i}:v]scale={maxResolutionMedia.Width}:{maxResolutionMedia.Height},setdar=dar={maxResolutionMedia.Ratio},setpts=PTS-STARTPTS[v{i}]; ");
             }
-
             for (var i = 0; i < mediaInfos.Count; i++)
             {
                 conversion.AddParameter(!mediaInfos[i].AudioStreams.Any() ? $"[v{i}]" : $"[v{i}][{i}:a]");
             }
 
             conversion.AddParameter($"concat=n={inputVideos.Length}:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\"");
+            conversion.AddParameter($"-aspect {maxResolutionMedia.Ratio}");
             conversion.SetOutput(output);
             return conversion.Start();
         }
