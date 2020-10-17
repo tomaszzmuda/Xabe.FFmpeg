58bb8ee1abd9140af019cefccc7281b7d5059011
delphianaus <35714916+delphianaus@users.noreply.github.com>
2020-10-17 15:21:32 +1100

    Add files via upload

---
+++ src/Xabe.FFmpeg/Conversion/Enums/AudoEncoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xabe.FFmpeg
+{
+    public enum AudioEncoder
+    {
+        copy,
+        lib_aac,
+        libmp3lame,
+        libvorbis,
+        libwavpack,
+        libtwolame,
+        flac
+    }
+}
---
+++ src/Xabe.FFmpeg/Conversion/Enums/Scaling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xabe.FFmpeg
+{
+    public enum Scaling
+    {
+        point,
+        bilinear,
+        bicubic,
+        spline16,
+        spline36,
+        lanczos
+    }
+}
