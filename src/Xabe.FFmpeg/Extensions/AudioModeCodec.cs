866df500dea7e3026cb6acfa6f43d6559be52656
delphianaus <35714916+delphianaus@users.noreply.github.com>
2020-10-17 15:17:33 +1100

    Add files via upload

---
+++ src/Xabe.FFmpeg/Extensions/AudioModeCodec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xabe.FFmpeg
+{
+    public static class AudioModeCodec
+    {
+        public static string TooParameter(this object AudioObject)
+        {
+            string result = "";
+            if (AudioObject is Type_libvorbis_Class @class)  result = @class.ToString();
+            if (AudioObject is Type_libtwolame_Class @class2) result = @class2.ToString();
+            if (AudioObject is Type_libwavpack_Class @class3) result = @class3.ToString();
+            if (AudioObject is Type_libfdk_aac_Class @class4) result = @class4.ToString();
+            if (AudioObject is Type_libmp3lame_Class @class5) result = @class5.ToString();
+            return result;
+        }
+    }
+}
---
+++ src/Xabe.FFmpeg/Extensions/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class Extensions
+    {
+        public static int ToInt(this string obj)
+        {
+            try
+            {
+                if (int.TryParse(obj.ToString(), out int result))
+                {
+                    return result;
+                }
+                else return 0;
+            }
+            catch (Exception ex)
+            {
+                
+                return 0;
+            }
+        }
+    }
+}
