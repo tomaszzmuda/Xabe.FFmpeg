0768bf86139d062e9f4f5957c4af262570559c05
Ben <ben.pearce@sky.com>
2021-05-29 20:34:21 +0100

    Add video data event handler

---
+++ src/Xabe.FFmpeg/Conversion/Events/VideoDataEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Xabe.FFmpeg.Events
+{
+    /// <summary>
+    ///     Info about conversion progress
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="args">Video data</param>
+    public delegate void VideoDataEventHandler(object sender, VideoDataEventArgs args);
+
+    /// <summary>
+    ///     Video data
+    /// </summary>
+    public class VideoDataEventArgs : EventArgs
+    {
+        /// <inheritdoc />
+        public VideoDataEventArgs(byte[] data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        ///     Binary video data
+        /// </summary>
+        public byte[] Data;
+    }
+}
