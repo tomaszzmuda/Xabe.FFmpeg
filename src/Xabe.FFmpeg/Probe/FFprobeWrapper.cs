5c5ebf0567dc73ab407cbad50ba2db24e9eee25d
Jonathan Boivin <djon2003@hotmail.com>
2023-02-01 16:43:04 -0500

    Fix of date time offset that was using the local time zone of the computer

--- src/Xabe.FFmpeg/Probe/FFprobeWrapper.cs
+++ src/Xabe.FFmpeg/Probe/FFprobeWrapper.cs
@@ -156,10 +156,10 @@ namespace Xabe.FFmpeg
             {
                 mediaInfo.Size = long.Parse(infos.format.size);
             }
-
-            if (!string.IsNullOrWhiteSpace(infos.format.tags?.creation_time) && DateTime.TryParse(infos.format.tags.creation_time, out var creationdate))
+            
+            if (!string.IsNullOrWhiteSpace(infos.format.tags?.creation_time) && DateTimeOffset.TryParse(infos.format.tags.creation_time, out var creationdate))
             {
-                mediaInfo.CreationTime = creationdate;
+                mediaInfo.CreationTime = creationdate.UtcDateTime;
             }
 
             mediaInfo.VideoStreams = PrepareVideoStreams(path, streams.Where(x => x.codec_type == "video"), infos.format);
