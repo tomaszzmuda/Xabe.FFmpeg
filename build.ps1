56bbf97053f73f51577e48b570af9c359707477b
Brita-bb <323116454+Brita-bb@users.noreply.github.com>
2026-08-31 13:21:47 +0200

    Remove Azure Pipelines definition

--- build.ps1
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dotnet restore
-cd Xabe.FFmpeg
-dotnet build /p:GenerateDocumentationFile=true
-dotnet pack --no-build /p:GenerateDocumentationFile=true
-Read-Host -Prompt "Press Enter to continue"
